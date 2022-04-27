30f7d0654e758662f0cd11218030838674a0e34d
Mister-718 <59221853+Mister-718@users.noreply.github.com>
2022-04-28 03:19:46 +0600

    created server, need to solve some issues

--- internal/http/server.go
+++ internal/http/server.go
@@ -1,1 +1,129 @@
 package http
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/chingizof/football-stats-golang/internal/models"
+	"github.com/chingizof/football-stats-golang/internal/store"
+	"github.com/gin-gonic/gin/render"
+	"github.com/go-chi/chi"
+)
+
+type Server struct {
+	ctx         context.Context
+	idleConnsCh chan struct{}
+	store       store.Store
+
+	Address string
+}
+
+func NewServer(ctx context.Context, address string, store store.Store) *Server {
+	return &Server{
+		ctx:         ctx,
+		idleConnsCh: make(chan struct{}),
+		store:       store,
+
+		Address: address,
+	}
+}
+
+func (s *Server) basicHandler() chi.Router {
+	r := chi.NewRouter()
+
+	// REST
+	// сущность/идентификатор
+
+	// /electronics/laptops
+	// /electronics/phones
+	// /clothes/t-shirts
+	r.Post("/electronics/laptops", func(w http.ResponseWriter, r *http.Request) {
+		player := new(models.Player)
+		if err := json.NewDecoder(r.Body).Decode(player); err != nil {
+			fmt.Fprintf(w, "Unknown err: %v", err)
+			return
+		}
+
+		s.store.Players().Create(r.Context(), player)
+	})
+	r.Get("/players", func(w http.ResponseWriter, r *http.Request) {
+		players, err := s.store.Players().All(r.Context())
+		if err != nil {
+			fmt.Fprintf(w, "Unknown err: %v", err)
+			return
+		}
+
+		render.JSON(w, r, players)
+	})
+	r.Get("/players/{id}", func(w http.ResponseWriter, r *http.Request) {
+		idStr := chi.URLParam(r, "id")
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			fmt.Fprintf(w, "Unknown err: %v", err)
+			return
+		}
+
+		player, err := s.store.Players().ByID(r.Context(), id)
+		if err != nil {
+			fmt.Fprintf(w, "Unknown err: %v", err)
+			return
+		}
+
+		render.JSON(w, r, player)
+	})
+	r.Put("/players", func(w http.ResponseWriter, r *http.Request) {
+		player := new(models.Player)
+		if err := json.NewDecoder(r.Body).Decode(player); err != nil {
+			fmt.Fprintf(w, "Unknown err: %v", err)
+			return
+		}
+
+		s.store.Players().Update(r.Context(), player)
+	})
+	r.Delete("/players/{id}", func(w http.ResponseWriter, r *http.Request) {
+		idStr := chi.URLParam(r, "id")
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			fmt.Fprintf(w, "Unknown err: %v", err)
+			return
+		}
+
+		s.store.Players().Delete(r.Context(), id)
+	})
+
+	return r
+}
+
+func (s *Server) Run() error {
+	srv := &http.Server{
+		Addr:         s.Address,
+		Handler:      s.basicHandler(),
+		ReadTimeout:  time.Second * 5,
+		WriteTimeout: time.Second * 30,
+	}
+	go s.ListenCtxForGT(srv)
+
+	log.Println("[HTTP] Server running on", s.Address)
+	return srv.ListenAndServe()
+}
+
+func (s *Server) ListenCtxForGT(srv *http.Server) {
+	<-s.ctx.Done() // блокируемся, пока контекст приложения не отменен
+
+	if err := srv.Shutdown(context.Background()); err != nil {
+		log.Printf("[HTTP] Got err while shutting down^ %v", err)
+	}
+
+	log.Println("[HTTP] Proccessed all idle connections")
+	close(s.idleConnsCh)
+}
+
+func (s *Server) WaitForGracefulTermination() {
+	// блок до записи или закрытия канала
+	<-s.idleConnsCh
+}
--- internal/store/store.go
+++ internal/store/store.go
@@ -1,1 +1,28 @@
 package store
+
+import (
+	"context"
+
+	"github.com/chingizof/football-stats-golang/internal/models"
+)
+
+type Store interface {
+	Players() PlayersRepository
+	Coaches() CoachesRepository
+}
+
+type PlayersRepository interface {
+	Create(ctx context.Context, player *models.Player) error
+	All(ctx context.Context) ([]*models.Player, error)
+	ByID(ctx context.Context, id int) (*models.Player, error)
+	Update(ctx context.Context, player *models.Player) error
+	Delete(ctx context.Context, id int) error
+}
+
+type CoachesRepository interface {
+	Create(ctx context.Context, coach *models.Coach) error
+	All(ctx context.Context) ([]*models.Coach, error)
+	ByID(ctx context.Context, id int) (*models.Coach, error)
+	Update(ctx context.Context, coach *models.Coach) error
+	Delete(ctx context.Context, id int) error
+}
