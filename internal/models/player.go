031c00e08a7907b58a74d5e3811c3f33b1b1b0ce
Mister-718 <59221853+Mister-718@users.noreply.github.com>
2022-04-28 03:02:14 +0600

    store instead of teams

--- internal/models/player.go
+++ internal/models/player.go
@@ -1,10 +1,19 @@
 package models
 
 type Player struct {
-	ID      int
-	Name    string
-	Surname string
+	ID      int    `json:"id"`
+	Name    string `json:"name"`
+	Surname string `json:"surname"`
+	Nation  string `json:"nation"`
+	Team    string `json:"team"`
 
-	Height int
-	Weight int
+	Position string `json:"position"`
+	Height   int    `json:"height"`
+	Weight   int    `json:"weight"`
+}
+
+type Coach struct {
+	ID      int    `json:"id"`
+	Name    string `json:"name"`
+	Surname string `json:"surname"`
 }
